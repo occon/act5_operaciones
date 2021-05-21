c6f181b65b6447c6dddeeb0f499022cabe79a829
Lydia <lydia101-lidi@hotmail.com>
2021-05-20 19:10:28 -0600

    [FEAT] Class Multiplicacion

---
+++ Multiplicacion.h
@@ -0,0 +1,19 @@
+//
+//  Multiplicacion.h
+//  Sumar,restar y multiplicar
+//
+//
+
+#ifndef Multiplicacion_h
+#define Multiplicacion_h
+#include "Operacion.h"
+
+class Multiplicacion: public Operacion{
+    public:
+        Multiplicacion();
+        void Operar();
+};
+
+    
+
+#endif /* Multiplicacion_h */
